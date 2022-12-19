c60f8bb38e8395b987bef40627cf3e0625aa094a
Brent Searl <brent.searl@enlighten.co.nz>
2022-12-19 13:56:49 +1300

    First basic controller

---
+++ Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Controllers/CharacterController.cs
@@ -0,0 +1,72 @@
+﻿using CharacterManagerAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CharacterManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private CMContext _context;
+
+        public CharacterController(CMContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> GetCharacters()
+        {
+            return Ok(await _context.Characters.ToListAsync());
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Character>> GetCharacter(int id)
+        {
+            var character = await _context.Characters.FindAsync(id);
+            if (character == null)
+            {
+                return BadRequest("Character was not found");
+            }
+            return Ok(character);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character character)
+        {
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Character>>> Update(Character updatedCharacter)
+        {
+            var character = await _context.Characters.FindAsync(updatedCharacter.Id);
+            if(character == null)
+            {
+                return BadRequest("Character not found");
+            }
+            character.Name = updatedCharacter.Name;
+            character.Level = updatedCharacter.Level;
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var character = await _context.Characters.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if(character == null)
+            {
+                return BadRequest("Character not found");
+            }
+            _context.Characters.Remove(character);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Controllers/WeatherForecastController.cs
+++ Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Controllers/WeatherForecastController.cs
@@ -3,21 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace CharacterManagerAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private CMContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, CMContext dbcontext)
         {
             _logger = logger;
+            _context = dbcontext;
         }
-
-        
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
     }
+
+
 }
