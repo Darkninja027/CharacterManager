ba2ae9a327dd61a7e8133c9f499f50b05f01287a
Brent Searl <brent.searl@outlook.com>
2022-12-20 20:27:19 +1300

    add sizes to races

--- Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Migrations/20221220072256_RaceSize.Designer.cs
+++ Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Migrations/20221220072256_RaceSize.Designer.cs
@@ -4,6 +4,7 @@ using CharacterManagerAPI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CharacterManagerAPI.Migrations
 {
     [DbContext(typeof(CMContext))]
-    partial class CMContextModelSnapshot : ModelSnapshot
+    [Migration("20221220072256_RaceSize")]
+    partial class RaceSize
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Models/Race.cs
+++ Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Models/Race.cs
@@ -2,13 +2,23 @@
 
 namespace CharacterManagerAPI.Models
 { 
+    public enum SizeEnum
+    {
+        Tiny = 1,
+        Small = 2,
+        Medium = 3,
+        Large = 4,
+        Huge = 5,
+        Gargantuan = 6,
+    }
     public class Race
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Character> Characters { get; set; } = new List<Character>(); 
+        //public ICollection<Character> Characters { get; set; } = new List<Character>(); 
         public int Speed { get; set; }
+        public SizeEnum Size { get; set; } = SizeEnum.Medium;
         public int Strength { get; set; } = 0;
         public int Dexterity { get; set; } = 0;
         public int Constitution { get; set; } = 0;
