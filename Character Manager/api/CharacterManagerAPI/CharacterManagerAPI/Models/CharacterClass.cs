04cd6e7cdd5b20e4e136a3ed66dc3b6d6caba09c
Brent Searl <brent.searl@outlook.com>
2023-04-17 12:04:39 +1200

    work on the class seed for the database

---
+++ Character Manager/api/CharacterManagerAPI/CharacterManagerAPI/Models/CharacterClass.cs
@@ -0,0 +1,14 @@
+﻿namespace CharacterManagerAPI.Models
+{
+    public enum HitDiceEnum
+    {
+        D6 = 1,
+        D8 = 2,
+        D10 = 3,
+        D12 = 4,
+    }
+    public class CharacterClass
+    {
+        public string Name { get; set; }
+    }
+}
